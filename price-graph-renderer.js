37ee607adc3a538173bde225ea784e12fd0f7652
Pieter Carton <pieterc.n@hotmail.com>
2023-03-10 17:50:13 +0100

    import chartjs, add example graph

--- price-graph-renderer.js
+++ price-graph-renderer.js
@@ -1,1 +1,26 @@
-console.log("Hello There!");
+import './node_modules/chart.js/dist/chart.umd.js'
+
+const data = [
+    {time: 0, price: 200},
+    {time: 1, price: 210},
+    {time: 2, price: 270},
+    {time: 3, price: 100}
+];
+
+const config = {
+    type: 'line',
+    data: {
+        labels: data.map(row => row.time.toString()),
+        datasets: [
+            { 
+                label: 'Frank Energie Prijs (EUR/KWh)',
+                data: data.map(row => row.price)
+            }
+        ]
+    }
+};
+
+new Chart(
+    document.getElementById('price-graph'),
+    config
+);
